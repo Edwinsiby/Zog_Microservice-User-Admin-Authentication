6472b0bd1251ba0ed74d91e869f11d001f450eae
edwinsiby <edwinsibyrajakumary@gmail.com>
2023-07-24 17:15:52 +0530

    done

--- cmd/main.go
+++ cmd/main.go
@@ -4,15 +4,18 @@ import (
 	"log"
 	"net"
 	"net/http"
-
-	"google.golang.org/grpc"
+	"os"
 
 	pb "service1/pb"
 	"service1/pkg/service"
+
+	"google.golang.org/grpc"
 )
 
 func main() {
 	myService := &service.MyService{}
+
+	logger := log.New(os.Stdout, "Main: ", log.LstdFlags)
 
 	grpcServer := grpc.NewServer()
 
@@ -20,12 +23,12 @@ func main() {
 
 	listener, err := net.Listen("tcp", ":5050")
 	if err != nil {
-		log.Fatalf("Failed to listen: %v", err)
+		logger.Fatalf("Failed to listen: %v", err)
 	}
-	log.Println("Admin&User-Authentication-Server is running on 5050")
+	logger.Println("Admin&User-Authentication-Server is running on :5050")
 	go grpcServer.Serve(listener)
 
 	if err := http.ListenAndServe(":8081", nil); err != nil {
-		log.Fatalf("Failed to start health check server: %v", err)
+		logger.Fatalf("Failed to start health check server: %v", err)
 	}
 }
